5173e24703af2b20eab3c91bac1d9d999e980179
tanakrit1998 <tanakrit.ja.59@ubu.ac.th>
2019-07-12 15:09:42 +0700

    add
    add assingmen-2

---
+++ galleryapp/index.ts
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from 'electron'
+
+let win = null
+
+function createWindow() {
+  win = new BrowserWindow({width: 500, height: 400})
+  win.setTitle('CSUBU Lecturers')
+  win.loadFile('index.html')
+  win.on('closed', () => { win = null })
+}
+
+app.on('ready', () => {
+    createWindow()
+})
+
+// สำหรับ macOS 
+app.on('window-all-closed', function () {
+  // ถ้าหน้าต่างทุกอันปิด
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// ถ้ากด icon ของ app ใหม่
+app.on('activate', function () {
+    console.log('เปิด activate แล้วน่ะ');
+  if (win == null) {
+    createWindow()
+  }
+})
